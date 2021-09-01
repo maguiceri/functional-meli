4e8db63bae743e537bf1e7612257308e184ea8df
magui <magui@Maguis-MacBook-Pro.local>
2021-09-01 17:34:42 -0300

    styles-input

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./styles.scss";
 
 function Navbar(props){
     //function handleCallBack(e){
         //props.handleCallBack(e.target.value)
     //}  esto seria si no lo hago en una sola linea
    return(
-       <>
-        <input type="text" onChange={(e) => props.handleCallBack(e.target.value)}></input>
-       </>
+       <div className="div">
+        <input className="input" type="text" onChange={(e) => props.handleCallBack(e.target.value)}></input>
+       </div >
    )
 }
 
